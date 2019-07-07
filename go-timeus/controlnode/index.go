7ef7974fbe4eadbe7c8747dbe913825ccb69dfdb
Timeus <info@timeus.org>
2019-07-07 22:44:44 +0300

    copyright

---
+++ go-timeus/controlnode/index.go
@@ -0,0 +1,15 @@
+// Copyright (C) 2019 TimeusLAB, Inc.
+// This file is part of go-timeus
+//
+// go-timeus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-timeus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-timeus.  If not, see <https://www.gnu.org/licenses/>.
